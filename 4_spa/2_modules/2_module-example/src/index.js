d249fab60c0570aaf2e9c6a415e944026c4f9069
FRickReich <frickreich@gmail.com>
2023-05-16 11:25:25 +0200

    Add documentation file

--- 4_spa/2_modules/2_module-example/src/index.js
+++ 4_spa/2_modules/2_module-example/src/index.js
@@ -1,1 +1,10 @@
-console.log("Hallo welt!");
+const randToken = require('rand-token');
+const Petals = require('petals');
+
+const token = randToken.generate(16);
+
+console.log(token);
+
+const petals = new Petals(`{"firstName": "John", "lastName": "Doe"}`);
+
+console.log(petals.JsonToXml());
