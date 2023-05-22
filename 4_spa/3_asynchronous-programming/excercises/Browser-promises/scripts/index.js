6ebadfe93c452644f78ccd4f9d84dc2a51f147e7
hnsreeny <hnsreeny@gmail.com>
2023-05-22 15:32:51 +0200

    Asynchronous Programming

--- 4_spa/3_asynchronous-programming/excercises/Browser-promises/scripts/index.js
+++ 4_spa/3_asynchronous-programming/excercises/Browser-promises/scripts/index.js
@@ -1,1 +1,22 @@
 // write your code here
+const modal = document.querySelector("#myModal");
+
+let promiseOfModal = new Promise((resolve) => {
+  window.setTimeout(() => {
+    resolve(modal);
+  }, 1000 * 5);
+});
+
+promiseOfModal.then((val) => {
+  console.log("User has been waiting for 5 seconds");
+  val.style.display = "block";
+});
+
+modal.addEventListener("click", (e) => {
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
