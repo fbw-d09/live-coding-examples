ab976ab4e3fc5a7180ded26d7b3b04ea58f3bc06
hnsreeny <hnsreeny@gmail.com>
2023-05-22 09:19:34 +0200

    Asynchronous Programming

---
+++ 4_spa/3_asynchronous-programming/promises/src/index.js
@@ -0,0 +1,7 @@
+// const funColor = () => {
+//   document.body.style.backgroundColor = "red";
+// };
+// setTimeout(funColor, 2000);
+setTimeout(() => {
+  document.body.style.backgroundColor = "red";
+}, 2000);
