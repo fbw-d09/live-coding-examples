aa312c6e06c832a960b1731e477e588ed1251e8c
hnsreeny <hnsreeny@gmail.com>
2023-05-23 11:55:53 +0200

    Async &  Await

--- 4_spa/5_async-await/index.js
+++ 4_spa/5_async-await/index.js
@@ -18,7 +18,33 @@ function getUser(id) {
   const user = users.find((userInfo) => userInfo.id == id);
   return Promise.resolve(user);
 }
-function getPosts(lastName) {
-  const userPosts = posts[lastName];
+function getPosts(name) {
+  const userPosts = posts[name];
   return Promise.resolve(userPosts);
 }
+function getGroups(name) {
+  const userGroups = groups[name];
+  return Promise.resolve(userGroups);
+}
+// getUser(1)
+//   // { id: 1, name: "Olga", lastName: "Char" }
+//   .then((valOne) => getPosts(valOne.lastName))
+//   // ["First info", "Cool, I'm on the beach"]
+//   .then((valTwo) => console.log(valTwo));
+
+// Async Await
+async function getUserData() {
+  try {
+    const user = await getUser(1);
+    const posts = await getPosts(user.name);
+    const groups = await getGroups(user.name);
+    console.log(i);
+    return [posts, groups];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUserData().then((data) => {
+  console.log(data);
+});
