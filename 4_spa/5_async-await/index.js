476b6addec683abc94dd2bead44ac993d708a7bf
hnsreeny <hnsreeny@gmail.com>
2023-05-23 11:08:37 +0200

    Async &  Await

---
+++ 4_spa/5_async-await/index.js
@@ -0,0 +1,15 @@
+// Database users
+const users = [
+  { id: 1, name: "Olga", lastName: "Char" },
+  { id: 2, name: "Nancy", lastName: "Co" },
+];
+// Database posts
+const posts = {
+  Olga: ["First info", "Cool, I'm on the beach"],
+  Nancy: ["🍸 with my bb", "🎂 happy bDay to me"],
+};
+// Database groups
+const groups = {
+  Olga: ["Drinking"],
+  Nancy: ["Eating"],
+};
