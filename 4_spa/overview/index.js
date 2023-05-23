8917cae835cee00eb9ce8e9fa6bf91eac973b342
hnsreeny <hnsreeny@gmail.com>
2023-05-23 13:48:38 +0200

    Async &  Await

--- 4_spa/overview/index.js
+++ 4_spa/overview/index.js
@@ -8,3 +8,24 @@ setTimeout(() => {
   console.log("I'm delay with 0ms");
 }, 0);
 console.log("last line");
+// mini task
+Promise.resolve({ name: "Zain" }).then((val) => console.log(val));
+const div = document.querySelector("#root");
+const users = ["Nancy", "Olga", "Zain", "Jack"];
+function getUsers() {
+  setTimeout(() => {
+    users.forEach((user) => {
+      const p = document.createElement("p");
+      p.innerText = user;
+      div.appendChild(p);
+    });
+  }, 1500);
+}
+function addUser(userName, callback) {
+  setTimeout(() => {
+    users.push(userName);
+    callback();
+  }, 2000);
+}
+// getUsers();
+addUser("Ali", getUsers);
