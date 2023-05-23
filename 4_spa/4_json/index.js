d99df4a3666f4f86ebde7986321781f4ab3ad2a0
hnsreeny <hnsreeny@gmail.com>
2023-05-23 10:53:15 +0200

    JSON

---
+++ 4_spa/4_json/index.js
@@ -0,0 +1,17 @@
+import data from "./data.json" assert { type: "json" };
+// JSON ->  JavaScript Object Notation
+
+const obj = {
+  name: "Hadi",
+  age: 33,
+};
+
+console.log("obj:", obj);
+// Convert from Js -> to JSON
+const stringify = JSON.stringify(obj);
+console.log("JSON", stringify);
+// Convert from JSON -> to Js
+const parsed = JSON.parse(stringify);
+// console.log(typeof parsed);
+console.log("parsed", parsed);
+console.log(data);
