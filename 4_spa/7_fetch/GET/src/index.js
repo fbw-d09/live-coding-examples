ba81fd9bccf0a5a33f71df7cc53ab217749f683a
hnsreeny <hnsreeny@gmail.com>
2023-05-25 09:55:35 +0200

    Fetch 🦴

--- 4_spa/7_fetch/GET/src/index.js
+++ 4_spa/7_fetch/GET/src/index.js
@@ -1,6 +1,9 @@
 // API: Application Programming Interface
 // kostenlos API: https://jsonplaceholder.typicode.com/
 
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 // ! Example using promises
 
 // ! Example using async await
