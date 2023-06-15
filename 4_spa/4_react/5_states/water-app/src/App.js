d86c996224dd13358faba71895c2d220a27ee0f4
FRickReich <frickreich@gmail.com>
2023-06-15 11:07:15 +0200

    Add water example

--- 4_spa/4_react/5_states/water-app/src/App.js
+++ 4_spa/4_react/5_states/water-app/src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
-import { Water } from './components/';
+import { Message, Water } from './components/';
 
 function App() {
+
+  const [ isVisible, setIsVisible ] = useState(true); // let isVisible = true;
+
   return (
     <div className="App">
       <Water/>
+
+      <br />
+      <br />
+
+      <Message
+        color="tomato"
+        initVisibility={ isVisible }
+      >
+        Hallo Welt!
+      </Message>
     </div>
   );
 }
--- 4_spa/4_react/5_states/water-app/src/components/Message/Message.jsx
+++ 4_spa/4_react/5_states/water-app/src/components/Message/Message.jsx
@@ -1,3 +1,33 @@
-export const Message = () => {
-    return(<></>)
+import React, { useState } from 'react';
+
+export const Message = ({ 
+    color, 
+    initVisibility, // initVisibility = true;
+    children
+}) => {
+    const visibilityState = initVisibility; // visibilityState = true;
+
+    const [ visible, setVisible ] = useState(visibilityState); // visible = true;
+
+    const handleVisibility = () =>
+    {
+        setVisible(false);
+    }
+
+    return(
+        <>
+            <div
+                style={{
+                    background: `${ color || "cyan" }`,
+                    border: "1px solid black",
+                    width: "200px",
+                    display: `${ visible ? "block" : "none" }`,
+                    margin: "0 auto"
+                }}
+            >
+                { children }
+                <button onClick={ handleVisibility }>x</button>
+            </div>
+        </>
+    )
 }
