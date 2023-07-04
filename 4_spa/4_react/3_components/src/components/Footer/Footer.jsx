065b72a08f1cb6a146bad26d1854f2c6cea07f77
FRickReich <frickreich@gmail.com>
2023-07-04 08:44:34 +0200

    Refactor example

--- 4_spa/4_react/3_components/src/components/Footer/Footer.jsx
+++ 4_spa/4_react/3_components/src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
+import PropTypes from 'prop-types';
 import './Footer.scss';
 
-export const Footer = () =>
+export const Footer = ({ owner }) =>
 {
     return(
         <div className="Footer">
             <p>
-                &copy; { new Date().getFullYear() } by Unserer Klasse :D
+                &copy; { new Date().getFullYear() } by { owner } :D
             </p>
         </div>
     )
